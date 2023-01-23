ab5ff63d69f3821e2ef629bf7b5e3fd5b3d8bad9
Daniel Brott <danbrott42@gmail.com>
2023-01-23 23:30:37 +0000

    updated functionality

---
+++ python/two_number_sum.py
@@ -0,0 +1,9 @@
+# Two Number Sum
+
+"""
+Write a function that takes in a non empty array of distinct integers representing a target sum. If any two numbers in
+the input array sum up to the target sum, the function should return them om am array in any order. If no two numbers
+sum up to the target, the function should return an empty array.
+"""
+
+g
