728db17c7a01e2df18bb62b591a086e2521cdfd3
Daniel Brott <danbrott42@gmail.com>
2023-04-22 18:06:53 -0700

    added description

--- python/merged_intervals.py
+++ python/merged_intervals.py
@@ -20,7 +20,19 @@ def merge_intervals(intervals):
     merged_intervals.append(current_interval)
     return merged_intervals
 
+"""
+The function merge_intervals takes a list of intervals represented as lists of two integers and returns a new list of merged intervals where overlapping intervals are combined into a single interval.
 
+The function first sorts the input intervals based on their start times using the sort method and a lambda function.
+
+It then initializes an empty list called merged_intervals and a variable called current_interval to the first interval in the sorted list.
+
+The function then loops through the remaining intervals in the list and checks if the current interval overlaps with the next interval. If there is no overlap, the current interval is added to the merged_intervals list and the current interval is updated to the next interval. If there is an overlap, the function updates the end time of the current interval to the maximum of its current end time and the end time of the next interval.
+
+Finally, the last interval is added to the merged_intervals list and the merged list is returned.
+"""
 if __name__ == '__main__':
     print(merge_intervals([[1, 2], [2, 5], [6, 7]])) # [[1, 5], [6, 7]]
     print(merge_intervals([[1, 2], [2, 5], [6, 7], [7, 10]])) # [[1, 5], [6, 10]
+
+
