2c00e636153b000f7906b233c0c7531095d22580
Daniel Brott <danbrott42@gmail.com>
2023-04-29 22:40:58 -0700

    test added to function

--- python/move_element_to_end.py
+++ python/move_element_to_end.py
@@ -2,6 +2,8 @@
 Write a function that takes in an array and an integer and mutates the array in place and puts all values that are equal to the target integer to the end of the array,
 """
 
+import unittest
+
 def move_to_end(arr, target):
     left = 0
     right = len(arr) - 1
@@ -14,5 +16,13 @@ def move_to_end(arr, target):
         left += 1
     return arr
 
-if __name__ == "__main__":
-    print(move_to_end([1, 2, 3, 4, 5, 6, 2, 2, 2], 2)) # output: [1, 6, 3, 4, 5, 2, 2, 2, 2]
+class TestMoveToEnd(unittest.TestCase):
+    def test_move_to_end(self):
+        arr = [1, 2, 3, 4, 5, 6, 6, 5, 5, 5, 5]
+
+        self.assertEqual(move_to_end(arr, 6), [1, 2, 3, 4, 5, 5, 5, 5, 5, 6, 6])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
