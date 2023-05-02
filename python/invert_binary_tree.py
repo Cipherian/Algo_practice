baf6e3f7b365a9706b9d123238d5f2a0a719d070
Daniel Brott <danbrott42@gmail.com>
2023-05-01 19:33:01 -0700

    tree inversion test

--- python/invert_binary_tree.py
+++ python/invert_binary_tree.py
@@ -1,7 +1,10 @@
 """
 Write a function that takes in a binary tree and inverts it.
 """
+import unittest
 # This is the class of the input binary tree.
+
+
 class BinaryTree:
     def __init__(self, value):
         self.value = value
@@ -23,22 +26,22 @@ def invertBinaryTree(tree):
     invertBinaryTree(tree.left)
     invertBinaryTree(tree.right)
 
-
-
-if __name__ == "__main__":
-    BT = BinaryTree(1)
-    BT.left = BinaryTree(2)
-    BT.right = BinaryTree(3)
-    BT.left.left = BinaryTree(4)
-    BT.left.right = BinaryTree(5)
-    BT.right.left = BinaryTree(6)
-    BT.right.right = BinaryTree(7)
-    print(BT)
-    invertBinaryTree(BT)
-    print(BT)
-
+class TestBinaryTree(unittest.TestCase):
+    def test_invert_binary_tree(self):
+        tree = BinaryTree(1)
+        tree.left = BinaryTree(2)
+        tree.right = BinaryTree(3)
+        tree.left.left = BinaryTree(4)
+        tree.left.right = BinaryTree(5)
+        tree.right.left = BinaryTree(6)
+        tree.right.right = BinaryTree(7)
+        invertBinaryTree(tree)
+        self.assertEqual(4, tree.right.right.value)
+        self.assertEqual(7, tree.left.left.value)
 
 
 
+if __name__ == '__main__':
+    unittest.main()
 
 
