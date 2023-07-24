aaef08dbefa664be029d147739b5e53494bb60dc
Daniel Brott <danbrott42@gmail.com>
2023-07-24 02:22:21 +0000

    added smallest difference tests

--- python/smallest_difference.py
+++ python/smallest_difference.py
@@ -2,6 +2,7 @@
 Write a function that takes in two non-empty lists of integers and finds the pair of numbers whose absolute difference
 is closest to zero. Returns a list of those two numbers
 """
+import unittest
 
 
 def smallest_difference(array_one, array_two):
@@ -27,7 +28,10 @@ def smallest_difference(array_one, array_two):
             j += 1
     return smallest_pair
 
-
+class Test(unittest.TestCase):
+    def test_smallest_difference(self):
+        self.assertEqual(smallest_difference([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [66, 11, 44, 22, 87, 66, 111, -111, -222, -55]), [10, 11])
+        self.assertLess(sum(smallest_difference([1, 2, 3, 4, 5, 6], [66, 11, 44, 22, 87])), sum([44, 7]))
+        self.assertGreater(sum(smallest_difference([1, 2, 3, 4], [6, 11, 44, 22])), sum([1, 6]))
 if __name__ == '__main__':
-    print(smallest_difference([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [66, 11, 44, 22, 87, 66, 111, -111, -222, -55]))
-    # [10, 11]
+    unittest.main()
