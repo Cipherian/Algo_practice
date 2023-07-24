cfd1981fa2a1851e1516d673f7caf154524882e6
Daniel Brott <danbrott42@gmail.com>
2023-07-24 02:14:45 +0000

    added sum queries tests

--- python/sum_queries_array.py
+++ python/sum_queries_array.py
@@ -4,7 +4,7 @@ Write a function that returns the total amount of waiting time for all the queri
 queries of durations [1, 4, 5], then the total waiting time if the queries were executed in the order of [5, 1, 4]
  would be (0 + 5), + (5 + 1) = 11.
 """
-
+import unittest
 
 def total_waiting_time(queries: list[int]) -> int:
     queries.sort()  # sorts the queries in ascending order
@@ -14,8 +14,11 @@ def total_waiting_time(queries: list[int]) -> int:
         total_time += waiting_time  # adding the waiting time to the total time
     return total_time
 
+class TestTotalWaitingTime(unittest.TestCase):
+    def test_total_waiting_time(self):
+        self.assertEqual(total_waiting_time([1, 4, 5]), 6)
+        self.assertEqual(total_waiting_time([1, 3, 4]), 5)
 
 if __name__ == "__main__":
-    print(total_waiting_time([1, 4, 5]))
-    print(total_waiting_time([5, 1, 4]))
-    print(total_waiting_time([1, 2, 3, 4, 5]))
+    unittest.main()
+
